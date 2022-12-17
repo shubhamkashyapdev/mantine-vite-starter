2a0d88fa5feb8d1fb6b2310dbe1e609e30ecbdd7
shubhamwebdesign <hex.media.1800@gmail.com>
2022-12-17 23:53:41 +0530

    project setup with eslint, prettier, husky, commitlint, mantine ui and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="">
+      <Button>Button</Button>;
+    </div>
+  )
+}
+
+export default App
