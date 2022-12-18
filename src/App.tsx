a2c95aae49ca2a1950fc200a7a1a5c763e204a31
shubhamwebdesign <hex.media.1800@gmail.com>
2022-12-18 17:22:27 +0530

    feat: vitest testing implemented with react-testing-library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Button } from '@material-tailwind/react'
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import { About, Home, NotFound } from '@/screens'
 
 function App() {
   return (
-    <div className="">
-      <Button>Button</Button>;
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   )
 }
 
